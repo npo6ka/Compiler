4615f0a0928407da3228c4cc474cfd08bd333428
npo6ka <lenchik.102@mail.ru>
2015-09-28 23:21:41 +0300

    edit comment

--- compiler/lexical_analyzer.cpp
+++ compiler/lexical_analyzer.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main() {
     fstream fs;
-    fs.open ("input.txt");
+    fs.open("input.txt");
     if (!fs.is_open()) {
         cout <<  "Error opening file" << endl;
         return -1;
     }
+
     filebuf* inbuf  = fs.rdbuf();
     char c = 0;
     while (c != EOF) {
@@ -18,20 +19,18 @@ int main() {
         cout << c;
     }
     cout << endl;
-
+    
     fs.close();
     return 1;
 }
 
 /*
-const ::= INT FLOAT
-AsExp ::= = *= /= += -=
-LoExp ::= || && | &
-Indef ::= ID 
-ResWr ::= if else for in return with 
-SpeCh ::= { } ( ) ; \0 \n _ 
-
-
+const ::= INT FLOAT 			        ('0'-'9', '+/-', '.')
+AsExp ::= = *= /= += -=                 ('=', '*', '/', '+', '-')
+LoExp ::= || && | &			            (|, &)
+Indef ::= ID 				            ('a'-'z', 'A'-'Z', '_', '$')
+ResWr ::= if else for in return with 	('i','e','f','i','r','w')
+SpeCh ::= { } ( ) ; \0 \n _ 	    	('{','}','(',')',';',' ','\n','\0')
 
 01010000
 hsdfsdfdfsdf d h g ddfg i sdfsd
