6d3602b70db09e07836cf44ed5b73fd45b1b52a0
npo6ka <lenchik.102@mail.ru>
2015-10-15 15:22:07 +0300

    edit enum

--- compiler/lexical_analyzer.cpp
+++ compiler/lexical_analyzer.cpp
@@ -13,62 +13,66 @@ unordered_map<string, int> ListResWord;
 
 enum {
     UNK_C = 0,
-    CONST = 1,
-    AS_OP = 2,
-    LO_OP = 3,
-    IDEN  = 4,
-    SP_SY = 5,
+    AS_OP = 1,
+    LO_OP = 2,
+    SP_SY = 3,
+    CONST = 4,
+    IDEN  = 5,
     ERR_C = 6,
     RES_W = 7,
 };
 
 enum {
-    //class const
-    INT    = 16,
-    FLOAT  = 17,
-	
-    //class Assignment operation
-    ASIG   = 32,
-    SUM    = 33,
-    DIFF   = 34,
-    MULT   = 35,
-    DIVI   = 36,
-	
-    //class Logical Expression
-    BN_AND = 48,
-    LO_AND = 49,
-    BN_OR  = 50,
-    LO_OR  = 51,
+    UNK    = 0xF + (UNK_C << 4), 
 
-    //class indificator
-    IND    = 64,
+    //class Assignment operation
+    ASIG   = 0x0 + (AS_OP << 4),
+    SUM    = 0x1 + (AS_OP << 4),
+    DIFF   = 0x2 + (AS_OP << 4),
+    MULT   = 0x3 + (AS_OP << 4),
+    DIVI   = 0x4 + (AS_OP << 4),
+
+    //class Logical Expression
+    BN_AND = 0x0 + (LO_OP << 4),
+    LO_AND = 0x1 + (LO_OP << 4),
+    BN_OR  = 0x2 + (LO_OP << 4),
+    LO_OR  = 0x3 + (LO_OP << 4),
 
     //class special symbol
-    LFB    = 80, //left curly brace
-    RFB    = 81, //right
-    LF_PR  = 82, //left parenthesis
-    RG_PR  = 83,
-    SEMIC  = 84, //semicolon
+    LFB    = 0x0 + (SP_SY << 4), //left curly brace
+    RFB    = 0x1 + (SP_SY << 4), //right
+    LF_PR  = 0x2 + (SP_SY << 4), //left parenthesis
+    RG_PR  = 0x3 + (SP_SY << 4),
+    SEMIC  = 0x4 + (SP_SY << 4), //semicolon
+
+    //class const
+    C_INT  = 0x0 + (CONST << 4),
+    C_FLT  = 0x1 + (CONST << 4),
+
+    //class indificator
+    IND    = 0x0 + (IDEN << 4),
 
     //class Error
-    ERR    = 96,
-    EX_ERR = 97, //end error
-    EXIT   = 98, //not add list
+    ERR    = 0x0 + (ERR_C << 4),
+    EX_ERR = 0x1 + (ERR_C << 4), //end error
+    EXIT   = 0x2 + (ERR_C << 4), //not add list
 
     //class reserved word
-    IF     = 112,
-    ELSE   = 113,
-    FOR    = 114,
-    IN     = 115,
-    RETURN = 116,
-    WITH   = 117, 
+    IF     = 0x0 + (RES_W << 4),
+    ELSE   = 0x1 + (RES_W << 4),
+    FOR    = 0x2 + (RES_W << 4),
+    IN     = 0x3 + (RES_W << 4),
+    RETURN = 0x4 + (RES_W << 4),
+    WITH   = 0x5 + (RES_W << 4),
+    INT    = 0x6 + (RES_W << 4),
+    FLOAT  = 0x7 + (RES_W << 4),
 };
 
 struct lexem {
     char id;
     string str;
 };
-
+//????? default val in cash table
 char* FillTable(const string str[], char *cash, const int num) { 
     for (int i = 0; i < num; i++) {
         for (int j = 0; j < str[i]._Mysize; j++) {
@@ -94,14 +98,15 @@ void AssignClass(const char* const cash, const char ch, int &ClassLex) {
 
 void init(char *cash) {
     string str[NUM_CLASS_LEX-1] = {
-        "0123456789.",                                            //1 - const
-        "=*+-/",                                                  //2 - Assignment operation
-        "|&",                                                     //3 - Logical Expression
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$", //4 - identificator
-        "{}(); \n",                                               //5 - special symbol & separator
+        "=*+-/",                                                  //1 - Assignment operation
+        "|&",                                                     //2 - Logical Expression
+        "{}(); \n",                                               //3 - special symbol & separator
+        "0123456789.",                                            //4 - const
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$", //5 - identificator
                                                                   //6 - errors 
     };                                                            //7 - Reserved Word
-    memset(cash, 6, sizeof(char)*SIZE_CASH_TABLE);
+    
+    memset(cash, SP_SY, sizeof(char)*SIZE_CASH_TABLE);
     FillTable(str, cash, NUM_CLASS_LEX-1);
     ListResWord.insert(pair<string, int> ("if",     IF    ));
     ListResWord.insert(pair<string, int> ("else",   ELSE  ));
@@ -109,20 +114,24 @@ void init(char *cash) {
     ListResWord.insert(pair<string, int> ("in",     IN    ));
     ListResWord.insert(pair<string, int> ("return", RETURN));
     ListResWord.insert(pair<string, int> ("with",   WITH  ));
+    ListResWord.insert(pair<string, int> ("int",   INT  ));
+    ListResWord.insert(pair<string, int> ("float",   FLOAT  ));
 }
 
 int HandleConst         (char ch, int stat, filebuf  &file, string &str) { // class 1
     static char tab[8][5] = {
 //      0-9   +/-     .      E     other
         { 1,  ERR,   2,     ERR,   ERR   }, // start
-        { 1,  INT,   3,     5,     INT   }, // int
-        { 4,  INT,   INT,   ERR,   INT   }, // .*
-        { 4,  INT,   INT,   5,     INT   }, // *.
-        { 4,  FLOAT, FLOAT, 5,     17    }, // float
+        { 1,  C_INT,   3,     5,   C_INT }, // int
+        { 4,  ERR,   ERR,   ERR,   ERR   }, // .*
+        { 4,  C_FLT, C_FLT, 5,     C_FLT }, // *.
+        { 4,  C_FLT, C_FLT, 5,     17    }, // float
         { 7,  6,     ERR,   ERR,   ERR   }, // E
         { 7,  ERR,   ERR,   ERR,   ERR   }, // +/-
-        { 7,  FLOAT, FLOAT, FLOAT, FLOAT }  // float
+        { 7,  C_FLT, C_FLT, C_FLT, C_FLT }  // float
     }; 
+
+
     if (ch >= '0' && ch <= '9') {
         stat = tab[stat][0];
     } else   if (ch == '+' || ch == '-') {
@@ -248,23 +257,22 @@ void ProcessingStatus(int &stat, int &ClassLex, string &str, list<lexem> &lst) {
 }
 
 void PrintLex(list<lexem> lst) {
-    char *str[RES_W] = {  "constant          ", 
-                          "assigment operator", 
+    char *str[RES_W] = {  "assigment operator", 
                           "logical operator  ", 
-                          "identificator     ", 
                           "special symbol    ", 
+                          "constant          ", 
+                          "identificator     ", 
                           "Error lexem       ", 
                           "reserved word     "};
-	char *output[7][6] = {
-		{"int    ",  "float  ", ""       , ""       , ""      , ""        },
-		{"=      ",  "+      ", "-      ", "*      ", "/     ", ""        },
-		{"&      ",  "&&     ", "|      ", "||     ", ""      , ""        },
-		{"       ",  ""       , ""       , ""       , ""      , ""        },
-		{"{      ",  "}      ", "(      ", ")      ", ";     ", ""        },
-		{""       ,  "       ", ""       , ""       , ""      , ""        },
-		{"if     ",  "else   ", "for    ", "in     ", "return", "with   " },
+    char *output[7][8] = {
+        {"=      ",  "+      ", "-      ", "*      ", "/      ", ""       , ""       , ""        },
+        {"&      ",  "&&     ", "|      ", "||     ", ""       , ""       , ""       , ""        },
+        {"{      ",  "}      ", "(      ", ")      ", ";      ", ""       , ""       , ""        },
+        {"int    ",  "float  ", ""       , ""       , ""       , ""       , ""       , ""        },		
+        {"       ",  ""       , ""       , ""       , ""       , ""       , ""       , ""        },
+        {""       ,  "       ", ""       , ""       , ""       , ""       , ""       , ""        },
+        {"if     ",  "else   ", "for    ", "in     ", "return ", "with   ", "INT    ", "FLOAT  " }
 	};
-	
 
     for (auto& it: lst) {
         cout <<  str[((it.id & 0xF0) >> 4)-1] << "   Lexem: " 
@@ -282,14 +290,13 @@ int main() {
     string buff = "";
     init(cash);
     int (*lexHandler[NUM_CLASS_LEX])(char ch, int stat, filebuf  &file, string &str) = 
-        {HandleConst, HandlerAssignment, HandlerLogical, HandlerId, HandlerSpecialSymbol, HandlerError};    
+        {HandlerAssignment, HandlerLogical, HandlerSpecialSymbol, HandleConst, HandlerId, HandlerError};    
     fs.open("input.txt");
     if (!fs.is_open()) {
         cout <<  "Error opening file" << endl;
         return -1;
     }
     filebuf* inbuf  = fs.rdbuf();
-
     do {
         symbol = inbuf->sbumpc();
         AssignClass(cash, symbol, ClassLexems);
@@ -310,7 +317,4 @@ int main() {
 4 ResWr ::= if else for in return with    ('i','e','f','i','r','w')       
 5 SpeCh ::= { } ( ) ;                     ('{','}','(',')',';')           ("{}(),;")
 6 Divid ::= \0 \n _                       (' ','\n','\0')                 (" ,\n\0")
-
-01010000
-hsdfsdfdfsdf d h g ddfg i sdfsd
 */
